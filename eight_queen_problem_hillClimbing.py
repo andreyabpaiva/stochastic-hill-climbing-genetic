e26e4f13c6b8db6bd3ab0f3c1143015e9801fc18
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-09-07 22:36:26 -0300

    correcting collisions function and stochastic_hill_climbing function added

--- eight_queen_problem_hillClimbing.py
+++ eight_queen_problem_hillClimbing.py
@@ -1,31 +1,59 @@
-def calculate_colisions(state: list) -> int:
+import random
+
+
+def calculate_collisions(state: list) -> int:
     """
-    Compute the number of colisions beetween queens in the board
+    Compute the number of collisions beetween queens in the board
 
     args:
         list: list of queen positions
 
     returns:
-        int: number of colisions
+        int: number of collisions
     """
-    colisions = 0
-    for queen_target_column in range(len(state) // 2):
-
-        queen = {
-            "column": queen_target_column,
-            "row": state[queen_target_column]
-        } # Queen position
-
-        for column, row in enumerate(state):
-            if column == queen_target_column:
-                continue
-            if (column - row == queen["column"] - queen["row"]) or (row + column == queen["column"] + queen["row"]):
-                colisions += 1
+    collisions = 0
+    for i in range(len(state)):
+        for j in range(i + 1, len(state)):
+            if state[i] == state[j] or abs(state[i] - state[j]) == abs(i - j):
+                collisions += 1
         
-    return colisions
+    return collisions
 
 
-state = [0, 1, 2, 3, 4, 5, 6, 7]
+def stochastic_hill_climbing(max_no_improve: int = 500) -> tuple:
+    """
+        Implementation of the Stochastic Hill Climbing algorithm for the 8 queen problem
 
-number_of_colisions = calculate_colisions(state)
-print(number_of_colisions)
+        args:
+            int: max number of iterations
+    """
+
+    state = [random.randint(0, 7) for _ in range(8)] # Initial Solution
+    current_collisions = calculate_collisions(state) # Initial Collisions count
+    no_improvement = 0 # No Improvements Count
+
+    while no_improvement < max_no_improve:
+        col = random.randint(0, 7)
+        min_collisions = current_collisions
+        best_state = state[:]
+
+        for row in range(8):
+            if state[col] == row: # Verify if the queen is already in this row
+                continue
+            new_state = state[:]
+            new_state[col] = row
+            new_collisions = calculate_collisions(new_state)
+            if new_collisions < min_collisions:
+                min_collisions = new_collisions
+                best_state = new_state
+        
+        if min_collisions < current_collisions: # There is a improvement
+            state = best_state
+            current_collisions = min_collisions
+            no_improvement = 0
+        else: 
+            no_improvement += 1
+        
+
+    return state, current_collisions
+
